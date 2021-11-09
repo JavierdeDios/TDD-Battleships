e1b79d9d35ae63a31c997c7f72c61e9d339cf1bc
MellacE98 <mellace1998@gmail.com>
2021-11-09 11:57:51 +0100

    Tests de la classe Ship
    Testejats els setters i getters de la classe ship, el constructor per defecte i un constructor que farem servir per inicialitzar directament.

--- src/test/java/BattleshipTests/ShipTests.java
+++ src/test/java/BattleshipTests/ShipTests.java
@@ -6,5 +6,55 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShipTests {
-   
+    Ship ship;
+
+    @BeforeEach
+    void setUpShip() {
+        ship = new Ship();
+    }
+
+    @Test
+    void setShipTest() {
+        assertEquals(null, ship.getM_x());
+        assertEquals(null, ship.getM_y());
+        assertEquals(' ', ship.getM_orientation());
+        assertEquals(0, ship.getM_length());
+    }
+
+    @Test
+    void SetterGetterXTest() {
+        ship.setM_x(5);
+        assertEquals(5, ship.getM_x());
+    }
+
+    @Test
+    void SetterGetterYTest() {
+        ship.setM_y(2);
+        assertEquals(2, ship.getM_y());
+    }
+
+    @Test
+    void SetterGetterOrientationTest() {
+        ship.setM_orientation('h'); //Horizontal
+        assertEquals('h', ship.getM_orientation());
+
+        ship.setM_orientation('v'); // Vertical
+        assertEquals('v', ship.getM_orientation());
+    }
+
+    @Test
+    void SetterGetterLengthTest() {
+        ship.setM_length(5);
+        assertEquals(5, ship.getM_length);
+    }
+
+    @Test
+    void initializeShipTest() {
+        ship = new Ship(5, 4, 'h', 3);
+
+        assertEquals(5, ship.getM_x());
+        assertEquals(4, ship.getM_y());
+        assertEquals('h', ship.getM_orientation());
+        assertEquals(3, getM_length());
+    }
 }
