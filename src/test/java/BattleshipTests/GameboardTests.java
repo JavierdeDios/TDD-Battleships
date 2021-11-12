99eff6b37254ca39ca6c3cf7bd107853058caf4e
MellacE98 <mellace1998@gmail.com>
2021-11-12 18:07:24 +0100

    Tests de Setters i Getters classe gameboard
    S'han afegit els tests per implementar els Setters i Getters de la classe gameboard, en aquesta classe encara falten tests.

--- src/test/java/BattleshipTests/GameboardTests.java
+++ src/test/java/BattleshipTests/GameboardTests.java
@@ -1,11 +1,41 @@
 package BattleshipTests;
 
 import BattleshipGame.Gameboard;
+import BattleshipGame.Cell;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameboardTests {
 
+    Gameboard tauler;
+
+    @BeforeEach
+    void setUpGameboard() {
+        tauler = new Gameboard();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 9})
+    void SetterGetterCellValueTest(int i) {
+        tauler.setCellValue(i, i, i); //(x, y, value)
+        assertEquals(i, tauler.getCellValue(i, i));
+    }
+
+    void SetterGetterCellShowTest() {
+        tauler.setCellValue(i, i, i); //(x, y, value)
+        assertEquals(i, tauler.getCellValue(i, i));
+    }
+
+    @Test
+    void SwitchShowCellTest() {
+        tauler.showCell(0, 0);
+        assertEquals(true, tauler.getshowCell());
+    }
+
+    
 }
